a8d1e83433fc9ee7e84f2ec5471b47648db7cd37
Alessandro Maestri <a.maestri@almaviva.it>
2026-01-09 12:15:15 +0100

    rTimelogger v0.8.4

--- Formula/rtimelogger.rb
+++ Formula/rtimelogger.rb
@@ -1,22 +1,22 @@
 class Rtimelogger < Formula
   desc "Rust-based CLI tool for personal time logging and tracking"
   homepage "https://github.com/umpire274/rTimelogger"
-  version "0.8.3"
+  version "0.8.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/umpire274/rTimelogger/releases/download/v#{version}/rtimelogger-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "c76aa326097f509df9b3149628bcfc99ff8f9133828ef87d4b4a13a93b56eabf"
+      sha256 "74d1f9f7607455032e280b4ea7a0ea5f5b886c9b5e432f2a4df9e775bc32d7de"
     elsif Hardware::CPU.arm?
       url "https://github.com/umpire274/rTimelogger/releases/download/v#{version}/rtimelogger-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "3353ea7b354c3dadad2c4b3abeaa1ea9a818af7c5fb235194daf405032eada66"
+      sha256 "5a12086aea1f81a16402a1384208960b105203747662a6041d6f58d0a8ca3d8c"
     end
   end
 
   on_linux do
     url "https://github.com/umpire274/rTimelogger/releases/download/v#{version}/rtimelogger-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "fa265d5ca253077ee7b11e8e66a669d874fbd85818c40501a271ba2eb5348328"
+    sha256 "7356610fc5693e145660609ea10dc9901a2682712461d3ea02726835d15fbb6f"
   end
 
   # facoltativo, aiuta `brew livecheck`
